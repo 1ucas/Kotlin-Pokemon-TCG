1801adbc9ebee592d31afb2c223eb024bee85c4b
Lucas Maciel - DTI <lucas.maciel@dtidigital.com.br>
2020-03-24 11:07:33 -0300

    Inclusão de baseURL default para a aplicação

--- libraries/network/src/main/java/pokemontcg/libraries/network/ApiClientBuilder.kt
+++ libraries/network/src/main/java/pokemontcg/libraries/network/ApiClientBuilder.kt
@@ -12,9 +12,10 @@ object ApiClientBuilder {
 
     private val baseClient = OkHttpClient()
     private val gsonDefault = GsonBuilder().setDateFormat("yyyy-MM-dd").create()
+    private val defaultUrl = "https://api.pokemontcg.io/v1/"
 
     fun <T> createServiceApi(responseType: Class<T>,
-                             baseUrl: String,
+                             baseUrl: String = defaultUrl,
                              gsonConfig: Gson = gsonDefault,
                              vararg interceptor: Interceptor): T {
         val clientBuilder = baseClient.newBuilder()
