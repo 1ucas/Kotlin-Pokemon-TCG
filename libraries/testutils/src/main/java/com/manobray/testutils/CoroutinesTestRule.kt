5c41a1a8dbb0001ab71f152604b8cb3ccebf18c6
Lucas Maciel <lucasramos.m@gmail.com>
2020-10-08 16:01:30 -0300

    Lint fix

--- libraries/testutils/src/main/java/com/manobray/testutils/CoroutinesTestRule.kt
+++ libraries/testutils/src/main/java/com/manobray/testutils/CoroutinesTestRule.kt
@@ -1,13 +1,16 @@
 package com.manobray.testutils
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
+@ExperimentalCoroutinesApi
 class CoroutineTestRule : TestWatcher() {
+
     val testDispatcher = TestCoroutineDispatcher()
 
     override fun starting(description: Description?) {
