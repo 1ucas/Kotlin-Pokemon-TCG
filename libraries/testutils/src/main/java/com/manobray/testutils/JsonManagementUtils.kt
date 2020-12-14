0b6bb7aa1fd93acb2adc6e580f17eb88721afa2b
Lucas <lucasramos.m@gmail.com>
2020-12-14 20:56:10 -0300

    Ajuste de testes

--- libraries/testutils/src/main/java/com/manobray/testutils/JsonManagementUtils.kt
+++ libraries/testutils/src/main/java/com/manobray/testutils/JsonManagementUtils.kt
@@ -3,7 +3,7 @@ package com.manobray.testutils
 import java.io.File
 
 fun loadJsonFromResources(fileName: String): String {
-    val url = ClassLoader.getSystemClassLoader().getResource(fileName)
+    val url = ClassLoader.getSystemResource(fileName)
     val file = File(url.path)
     return file.readText()
 }
